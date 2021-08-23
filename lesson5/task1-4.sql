6b9f1eb0aeb509170ca4fcd9b2d3b196b7f4f4ef
Сергей Плешаков <support@pleshakov.org>
2021-08-23 11:58:19 +0500

    Clear master branch

--- lesson5/task1-4.sql
+++
@@ -1,5 +0,0 @@
--- »з таблицы users необходимо извлечь пользователей, родившихс€ в августе и мае.
--- ћес€цы заданы в виде списка английских названий (may, august)
-SELECT CONCAT(firstname, ' ', lastname) AS FIO, date_format(birthday, '%d %M')
-    FROM users u INNER JOIN profiles p ON u.id=p.user_id
-    WHERE lower(date_format(birthday, '%M')) IN ('may', 'august') 
--- lesson5/task2-1.sql
+++
@@ -1,3 +0,0 @@
--- Подсчитайте средний возраст пользователей в таблице users.
-SELECT AVG(ROUND(DATEDIFF(NOW(), birthday) / 365.25))
-    FROM profiles p INNER JOIN users u ON u.id=p.user_id 
